239eac3be535f1f849b1a0361641be943ec40d38
Luis Avila <a2018117998@estudiantes.upsa.edu.bo>
2020-08-29 13:03:25 -0400

    Proyecto base con modelo

---
+++ APIStudentSecurity/APIStudentSecurity/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace APIStudentSecurity.Models
+{
+    using System.Data.Entity;
+    public class DataContext: DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+            {
+            
+            }
+    }
+}
